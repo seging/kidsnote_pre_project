82539c68224866377d1a21d2f2a34a8922265a3e
seging <lksgfj93@naver.com>
2024-07-15 13:59:14 +0900

    Initial commit: 프로젝트 생성

---
+++ KidsNoteGoogleBook/KidsNoteGoogleBook/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  KidsNoteGoogleBook
+//
+//  Created by 이승기 on 7/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
